fbbb786477aa26049834132a42d8dea253d642b8
Karan Sharma <karan.sharma.2@team.telstra.com>
2021-05-08 01:08:30 +1000

    ViewBinding

--- app/src/main/java/com/karansyd4/pokedex/MainActivity.kt
+++ app/src/main/java/com/karansyd4/pokedex/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.karansyd4.pokedex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.karansyd4.pokedex.databinding.MainActivityBinding
 import com.karansyd4.pokedex.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
 }
--- app/src/main/java/com/karansyd4/pokedex/ui/main/MainFragment.kt
+++ app/src/main/java/com/karansyd4/pokedex/ui/main/MainFragment.kt
@@ -1,12 +1,12 @@
 package com.karansyd4.pokedex.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.karansyd4.pokedex.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.karansyd4.pokedex.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -14,11 +14,16 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var binding: MainFragmentBinding
+
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MainFragmentBinding.inflate(LayoutInflater.from(context))
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
