55212cb6bd1126e85bd177c4613008ac30a66027
Karan Sharma <karansyd4@gmail.com>
2021-08-19 12:02:57 +1000

    Adding Roboto fonts

--- app/src/main/java/com/karansyd4/pokedex/theme/Type.kt
+++ app/src/main/java/com/karansyd4/pokedex/theme/Type.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.karansyd4.pokedex.R
 
-// Font 1
 private val Montserrat = FontFamily(
     Font(R.font.montserrat_light, FontWeight.Light),
     Font(R.font.montserrat_regular, FontWeight.Normal),
@@ -21,47 +20,53 @@ private val Karla = FontFamily(
     Font(R.font.karla_bold, FontWeight.Bold)
 )
 
+private val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium)
+)
+
 val Typography = Typography(
     h1 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 96.sp,
         fontWeight = FontWeight.Light,
         lineHeight = 117.sp,
         letterSpacing = (-1.5).sp
     ),
     h2 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 60.sp,
         fontWeight = FontWeight.Light,
         lineHeight = 73.sp,
         letterSpacing = (-0.5).sp
     ),
     h3 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 48.sp,
         fontWeight = FontWeight.Normal,
         lineHeight = 59.sp
     ),
     h4 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 30.sp,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 37.sp
     ),
     h5 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 24.sp,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 29.sp
     ),
     h6 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 20.sp,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 24.sp
     ),
     subtitle1 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 16.sp,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 24.sp,
@@ -82,14 +87,14 @@ val Typography = Typography(
         letterSpacing = 0.15.sp
     ),
     body2 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 14.sp,
         fontWeight = FontWeight.Medium,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     button = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 14.sp,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 16.sp,
@@ -103,7 +108,7 @@ val Typography = Typography(
         letterSpacing = 0.4.sp
     ),
     overline = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Roboto,
         fontSize = 12.sp,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 16.sp,
